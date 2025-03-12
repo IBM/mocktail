d2d70c22aa8921031667f7e58d2fc45583d0c748
Mateus Devino <mdevino@ibm.com>
2025-03-12 16:31:51 -0300

    Add missing HTTP status codes
    Signed-off-by: Mateus Devino <mdevino@ibm.com>

--- mocktail/src/response.rs
+++ mocktail/src/response.rs
@@ -124,11 +124,72 @@ impl StatusCode {
 }
 
 impl StatusCode {
+    pub const CONTINUE: StatusCode = StatusCode(NonZeroU16::new(100).unwrap());
+    pub const SWITCHING_PROTOCOLS: StatusCode = StatusCode(NonZeroU16::new(101).unwrap());
+    pub const PROCESSING: StatusCode = StatusCode(NonZeroU16::new(102).unwrap());
+    pub const EARLY_HINTS: StatusCode = StatusCode(NonZeroU16::new(103).unwrap());
+
     pub const OK: StatusCode = StatusCode(NonZeroU16::new(200).unwrap());
-    pub const NOT_FOUND: StatusCode = StatusCode(NonZeroU16::new(404).unwrap());
+    pub const CREATED: StatusCode = StatusCode(NonZeroU16::new(201).unwrap());
+    pub const ACCEPTED: StatusCode = StatusCode(NonZeroU16::new(202).unwrap());
+    pub const NON_AUTHORITATIVE_INFORMATION: StatusCode = StatusCode(NonZeroU16::new(203).unwrap());
+    pub const NO_CONTENT: StatusCode = StatusCode(NonZeroU16::new(204).unwrap());
+    pub const RESET_CONTENT: StatusCode = StatusCode(NonZeroU16::new(205).unwrap());
+    pub const PARTIAL_CONTENT: StatusCode = StatusCode(NonZeroU16::new(206).unwrap());
+    pub const MULTI_STATUS: StatusCode = StatusCode(NonZeroU16::new(207).unwrap());
+    pub const ALREADY_REPORTED: StatusCode = StatusCode(NonZeroU16::new(208).unwrap());
+    pub const IM_USED: StatusCode = StatusCode(NonZeroU16::new(226).unwrap());
+
+    pub const MULTIPLE_CHOICES: StatusCode = StatusCode(NonZeroU16::new(300).unwrap());
+    pub const MOVED_PERMANENTLY: StatusCode = StatusCode(NonZeroU16::new(301).unwrap());
+    pub const FOUND: StatusCode = StatusCode(NonZeroU16::new(302).unwrap());
+    pub const SEE_OTHER: StatusCode = StatusCode(NonZeroU16::new(303).unwrap());
+    pub const NOT_MODIFIED: StatusCode = StatusCode(NonZeroU16::new(304).unwrap());
+    pub const USE_PROXY: StatusCode = StatusCode(NonZeroU16::new(305).unwrap());
+    pub const TEMPORARY_REDIRECT: StatusCode = StatusCode(NonZeroU16::new(307).unwrap());
+    pub const PERMANENT_REDIRECT: StatusCode = StatusCode(NonZeroU16::new(308).unwrap());
+
     pub const BAD_REQUEST: StatusCode = StatusCode(NonZeroU16::new(400).unwrap());
+    pub const UNAUTHORIZED: StatusCode = StatusCode(NonZeroU16::new(401).unwrap());
+    pub const PAYMENT_REQUIRED: StatusCode = StatusCode(NonZeroU16::new(402).unwrap());
+    pub const FORBIDDEN: StatusCode = StatusCode(NonZeroU16::new(403).unwrap());
+    pub const NOT_FOUND: StatusCode = StatusCode(NonZeroU16::new(404).unwrap());
+    pub const METHOD_NOT_ALLOWED: StatusCode = StatusCode(NonZeroU16::new(405).unwrap());
+    pub const NOT_ACCEPTABLE: StatusCode = StatusCode(NonZeroU16::new(406).unwrap());
+    pub const PROXY_AUTHENTICATION_REQUIRED: StatusCode = StatusCode(NonZeroU16::new(407).unwrap());
+    pub const REQUEST_TIMEOUT: StatusCode = StatusCode(NonZeroU16::new(408).unwrap());
+    pub const CONFLICT: StatusCode = StatusCode(NonZeroU16::new(409).unwrap());
+    pub const GONE: StatusCode = StatusCode(NonZeroU16::new(410).unwrap());
+    pub const LENGTH_REQUIRED: StatusCode = StatusCode(NonZeroU16::new(411).unwrap());
+    pub const PRECONDITION_FAILED: StatusCode = StatusCode(NonZeroU16::new(412).unwrap());
+    pub const PAYLOAD_TOO_LARGE: StatusCode = StatusCode(NonZeroU16::new(413).unwrap());
+    pub const URI_TOO_LONG: StatusCode = StatusCode(NonZeroU16::new(414).unwrap());
+    pub const UNSUPPORTED_MEDIA_TYPE: StatusCode = StatusCode(NonZeroU16::new(415).unwrap());
+    pub const RANGE_NOT_SATISFIABLE: StatusCode = StatusCode(NonZeroU16::new(416).unwrap());
+    pub const EXPECTATION_FAILED: StatusCode = StatusCode(NonZeroU16::new(417).unwrap());
+    pub const IM_A_TEAPOT: StatusCode = StatusCode(NonZeroU16::new(418).unwrap());
+    pub const MISDIRECTED_REQUEST: StatusCode = StatusCode(NonZeroU16::new(421).unwrap());
+    pub const UNPROCESSABLE_ENTITY: StatusCode = StatusCode(NonZeroU16::new(422).unwrap());
+    pub const LOCKED: StatusCode = StatusCode(NonZeroU16::new(423).unwrap());
+    pub const FAILED_DEPENDENCY: StatusCode = StatusCode(NonZeroU16::new(424).unwrap());
+    pub const TOO_EARLY: StatusCode = StatusCode(NonZeroU16::new(425).unwrap());
+    pub const UPGRADE_REQUIRED: StatusCode = StatusCode(NonZeroU16::new(426).unwrap());
+    pub const PRECONDITION_REQUIRED: StatusCode = StatusCode(NonZeroU16::new(428).unwrap());
+    pub const TOO_MANY_REQUESTS: StatusCode = StatusCode(NonZeroU16::new(429).unwrap());
+    pub const REQUEST_HEADER_FIELDS_TOO_LARGE: StatusCode = StatusCode(NonZeroU16::new(431).unwrap());
+    pub const UNAVAILABLE_FOR_LEGAL_REASONS: StatusCode = StatusCode(NonZeroU16::new(451).unwrap());
+
     pub const INTERNAL_SERVER_ERROR: StatusCode = StatusCode(NonZeroU16::new(500).unwrap());
-    // TODO: add additional
+    pub const NOT_IMPLEMENTED: StatusCode = StatusCode(NonZeroU16::new(501).unwrap());
+    pub const BAD_GATEWAY: StatusCode = StatusCode(NonZeroU16::new(502).unwrap());
+    pub const SERVICE_UNAVAILABLE: StatusCode = StatusCode(NonZeroU16::new(503).unwrap());
+    pub const GATEWAY_TIMEOUT: StatusCode = StatusCode(NonZeroU16::new(504).unwrap());
+    pub const HTTP_VERSION_NOT_SUPPORTED: StatusCode = StatusCode(NonZeroU16::new(505).unwrap());
+    pub const VARIANT_ALSO_NEGOTIATES: StatusCode = StatusCode(NonZeroU16::new(506).unwrap());
+    pub const INSUFFICIENT_STORAGE: StatusCode = StatusCode(NonZeroU16::new(507).unwrap());
+    pub const LOOP_DETECTED: StatusCode = StatusCode(NonZeroU16::new(508).unwrap());
+    pub const NOT_EXTENDED: StatusCode = StatusCode(NonZeroU16::new(510).unwrap());
+    pub const NETWORK_AUTHENTICATION_REQUIRED: StatusCode = StatusCode(NonZeroU16::new(511).unwrap());
 }
 
 impl Default for StatusCode {
