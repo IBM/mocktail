1471e33e8c2f3d49ecc8c1998f60e8a7f81b8676
declark1 <44146800+declark1@users.noreply.github.com>
2025-03-18 10:43:35 -0700

    Add service request validation and tests
    Signed-off-by: declark1 <44146800+declark1@users.noreply.github.com>

---
+++ mocktail-tests/tests/misc/validation.rs
@@ -0,0 +1,44 @@
+use anyhow::Error;
+use mocktail::prelude::*;
+use test_log::test;
+
+#[test(tokio::test)]
+async fn test_grpc_service() -> Result<(), Error> {
+    let server = MockServer::new("test").grpc();
+    server.start().await?;
+
+    let client = reqwest::Client::builder().http2_prior_knowledge().build()?;
+
+    // Invalid method
+    let response = client.get(server.url("/hello")).send().await?;
+    assert_eq!(response.status(), http::StatusCode::METHOD_NOT_ALLOWED);
+    assert!(response
+        .headers()
+        .get("allow")
+        .is_some_and(|value| value == "POST"));
+
+    // Invalid content-type
+    let response = client.post(server.url("/hello")).send().await?;
+    assert_eq!(response.status(), http::StatusCode::UNSUPPORTED_MEDIA_TYPE);
+    assert!(response
+        .headers()
+        .get("accept-post")
+        .is_some_and(|value| value == "application/grpc"));
+
+    Ok(())
+}
+
+#[test(tokio::test)]
+async fn test_http_service() -> Result<(), Error> {
+    let server = MockServer::new("test");
+    server.start().await?;
+
+    let client = reqwest::Client::builder().http2_prior_knowledge().build()?;
+
+    // Invalid method
+    let response = client.patch(server.url("/hello")).send().await?;
+    assert_eq!(response.status(), http::StatusCode::METHOD_NOT_ALLOWED);
+    assert!(response.headers().get("allow").is_some());
+
+    Ok(())
+}
